adc56dfbb0628c0bd6c00fcb38b033788960a647
Nikesh Ojha <nikeshojha71@gmail.com>
2026-04-13 17:37:10 +0545

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/Dev-Personal-Portfolio/", // Match repository name exactly
+  base: "/", 
   publicDir: "public",
 });
